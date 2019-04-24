ac039ba00eb0200794b6ee4184b5c9fa06368414
obb12 <omar.bb@hotmail.fi>
2019-04-24 08:59:52 +0300

    translations in separate files

---
+++ src/translations/en.js
@@ -0,0 +1,34 @@
+export const en = {
+  message: {
+    open_map: 'Open the map view?',
+    open: 'Open',
+    app: 'SATAKOLKYT map application',
+    reserve: 'reserve a strand ',
+    claim: 'report a cleaned strand',
+    reserve_clean: 'reserve a strand to clean up ',
+    date_start: 'date for the start',
+    time_clean: 'date for the cleaning ',
+    organizer_clean: 'organizer of the cleaning',
+    clean_count: 'how many people participated in the cleaning',
+    litter_count: 'how much litter did you find',
+    date_end: 'date for the end',
+    time_end: 'time for the end',
+    time_start: 'time for the start',
+    type_label: 'which kind of clean-up event',
+    type_open: 'event that is open to everyone',
+    type_private: 'private clean-up event',
+    organizer: 'organizer',
+    name: 'name',
+    phonenumber: 'phonenumber',
+    email: 'email',
+    contact: 'contact person',
+    close: 'close',
+    claim_clean: 'report a cleaned strand',
+    trash_little: 'almost nothing at all',
+    trash_small: 'about one plastic bag',
+    trash_medium: 'about one refuse sack',
+    trash_large: 'many refuse sacks',
+    trash_left:
+      'Did you leave trash sacks on the strand which should be removed?'
+  }
+}
---
+++ src/translations/fi.js
@@ -0,0 +1,30 @@
+export const fi = {
+  message: {
+    open_map: 'Avataanko Karttanäkymä?',
+    open: 'Avaa',
+    app: 'SATAKOLKYT karttasovellus',
+    reserve: 'varaa ranta',
+    claim: 'ilmoita ranta siivotuksi',
+    reserve_clean: 'Varaa ranta siivoukseen',
+    date_start: 'Päivämäärä aloitukselle',
+    date_end: 'Päivämäärä lopetukselle',
+    type_label: 'Minkäläiset talkoot',
+    time_end: 'kellonaika lopetukselle',
+    time_start: 'kellonaika aloitukselle',
+    type_open: 'avoimet talkoot',
+    type_private: 'oman porukan talkoot',
+    organizer: 'Järjestävän taho',
+    name: 'nimi',
+    phonenumber: 'puhelinnumero',
+    email: 'sähköposti',
+    contact: 'yhteyshenkilö',
+    close: 'sulje',
+    claim_clean: 'Ilmoita ranta siivotuksi',
+    trash_little: 'ei juuri lainkaan',
+    trash_small: 'muovipussillinen',
+    trash_medium: 'jätesäkillinen',
+    trash_large: 'monta jätesäkillistä',
+    trash_left:
+      'Did you leave trash sacks on the strand which should be removed?'
+  }
+}
